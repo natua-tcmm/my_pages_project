f7df3704520a3f04271797af1cc3b083ac6b2f52
natua-tcmm <natua.tcmm@gmail.com>
2023-08-31 00:54:03 +0900

    [update] ImportExportを可能に

--- my_apps/admin.py
+++ my_apps/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import SongDataC,SongDataO,GameDataB2023
 from import_export import resources
-from import_export.admin import ImportMixin
+from import_export.admin import ImportExportMixin
 
-admin.site.register(SongDataC)
-admin.site.register(SongDataO)
+# admin.site.register(SongDataC)
+# admin.site.register(SongDataO)
 # admin.site.register(GameDataB2023)
 
 class GameDataB2023Resource(resources.ModelResource):
@@ -12,5 +12,21 @@ class GameDataB2023Resource(resources.ModelResource):
         model = GameDataB2023
 
 @admin.register(GameDataB2023)
-class GameDataB2023Admin(ImportMixin, admin.ModelAdmin):
+class GameDataB2023Admin(ImportExportMixin, admin.ModelAdmin):
     resource_class = GameDataB2023Resource
+
+class SongDataCResource(resources.ModelResource):
+    class Meta:
+        model = SongDataC
+
+@admin.register(SongDataC)
+class SongDataCAdmin(ImportExportMixin, admin.ModelAdmin):
+    resource_class = SongDataCResource
+
+class SongDataOResource(resources.ModelResource):
+    class Meta:
+        model = SongDataO
+
+@admin.register(SongDataO)
+class SongDataOAdmin(ImportExportMixin, admin.ModelAdmin):
+    resource_class = SongDataOResource
