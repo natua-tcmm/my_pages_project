87f23372f5c03f019cce2e0ec4b01ae4214afc87
natua-tcmm <natua.tcmm@gmail.com>
2024-02-04 20:55:05 +0900

    [other]collectstatic

---
+++ static/random_tools/script.js
@@ -0,0 +1,51 @@
+// IDをformは#ajax-xxx、queryは#query-xxxにする
+
+// 送信時の処理
+$('#ajax-unicode').on('submit', function (e) {
+
+    ajax_send(e, "unicode",$('#query-unicode').val())
+    .done(function (response) {
+
+        $("#loading-text").css("display", "none");
+        console.log(response.result);
+
+        // 今表示されてるやつを消す
+        $("#unicode-table-wrapper").children().remove();
+
+        // 新しく表示する
+        $("#unicode-table-wrapper").prepend(response.result);
+
+    })
+    .fail(function () {
+        alert("Ajax通信に失敗しました(´・ω・`)")
+    });
+
+});
+
+
+
+// ------------------------
+
+var ajax_send = function (e, type, query) {
+
+    $("#loading-text").css("display", "");
+
+    // 通常の送信処理を止める
+    if (e) {
+        e.preventDefault();
+    }
+
+    // 送信する
+    return $.ajax({
+        'url': send_url,
+        'type': 'POST',
+        'data': {
+            "csrfmiddlewaretoken": csrfmiddlewaretoken,
+            "query": query,
+            "request_time": new Date().getTime(),
+            "type":type,
+        },
+        'dataType': 'json',
+    })
+
+}
